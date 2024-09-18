f49c6483d62fc49adced7a73f0054fee098d4baf
pavithra.holla <pavithra.holla@niceincontact.com>
2024-09-18 15:32:49 -0600

    Rotate List

---
+++ src/main/java/medium/linkedList/RotateList.java
@@ -0,0 +1,50 @@
+package medium.linkedList;
+
+import utils.ListNode;
+
+public class RotateList {
+
+    /**
+     * Time Complexity - o(n)
+     * Space Complexity - o(1)
+     * @param head
+     * @param k
+     * @return
+     */
+
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null) return null;
+        if(head.next == null || k == 0) return head;
+        int length = 1;
+
+        ListNode temp = head;
+
+        while(temp.next != null) {
+            temp = temp.next;
+            length++;
+        }
+
+        //Circular here - end of temp is connected to head
+        temp.next = head;
+
+        int numOfRotations = k%length;
+        //Calculate effective rotations
+        if(numOfRotations == 0) return head;
+        numOfRotations = length - numOfRotations;
+
+       while (numOfRotations > 0) {
+           temp = temp.next;
+           numOfRotations--;
+       }
+
+
+
+       //The item before the head
+       head = temp.next;
+       //Connect the item to null and make it a tail
+       temp.next = null;
+
+
+       return head;
+    }
+}
---
+++ src/test/java/medium/linkedList/RotateListTest.java
@@ -0,0 +1,43 @@
+package medium.linkedList;
+
+import org.junit.Test;
+import utils.ListNode;
+
+
+public class RotateListTest {
+
+    RotateList rotateList = new RotateList();
+
+    @Test
+    public void testRotateList() {
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(3);
+        listNode.next.next.next = new ListNode(4);
+        listNode.next.next.next.next = new ListNode(5);
+
+        ListNode rotateListNew = rotateList.rotateRight(listNode, 2);
+
+        assert rotateListNew != null;
+        assert rotateListNew.val == 4;
+        assert rotateListNew.next.val == 5;
+        assert rotateListNew.next.next.val == 1;
+        assert rotateListNew.next.next.next.val == 2;
+        assert rotateListNew.next.next.next.next.val == 3;
+        assert rotateListNew.next.next.next.next.next == null;
+
+
+        listNode = new ListNode(0);
+        listNode.next = new ListNode(1);
+        listNode.next.next = new ListNode(2);
+
+        rotateListNew = rotateList.rotateRight(listNode, 4);
+
+        assert rotateListNew != null;
+        assert rotateListNew.val == 2;
+        assert rotateListNew.next.val == 0;
+        assert rotateListNew.next.next.val == 1;
+        assert rotateListNew.next.next.next == null;
+
+    }
+}
