fe3bf15d8077d9e449f60f69bd92776fc40984d5
pavithra.holla <pavithra.holla@niceincontact.com>
2025-02-09 21:35:38 -0700

    More COmbinations

--- src/main/java/medium/backtracking/CombinationSumII.java
+++ src/main/java/medium/backtracking/CombinationSumII.java
@@ -1,32 +1,49 @@
 package medium.backtracking;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class CombinationSumII {
+    /**
+     * O(N*2^n) + nlogn
+     * @param candidates
+     * @param target
+     * @return
+     */
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        //nlogn
         Arrays.sort(candidates);//Sort them first
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> res = new LinkedList<>();//Use set here
 
-        backtrack(candidates, target, target,res, 0, new ArrayList<>() );
-        return res;
+        backtrack(candidates, target, res, 0, new ArrayList<>());
+        return new ArrayList<>(res);
     }
 
-    private void backtrack(int[] candidates, int remaining, int target, List<List<Integer>> res, int idx, List<Integer> combination) {
+    /**
+     * n*2^n
+     * @param candidates
+     * @param remaining
+     * @param res
+     * @param idx
+     * @param combination
+     */
+    private void backtrack(int[] candidates, int remaining, List<List<Integer>> res, int idx, List<Integer> combination) {
 
-        if(remaining == 0){
+        if (remaining == 0) {
             res.add(new ArrayList<>(combination));
             return;
         }
 
-        if(remaining < 0 || idx > candidates.length)
+        if (remaining < 0 || idx > candidates.length)
             return;
 
-        for(int i=idx; i<candidates.length && candidates[i] <= remaining; i++){
+        for (int i = idx; i < candidates.length && candidates[i] <= remaining; i++) {
+            if(i > idx && candidates[i - 1] == candidates[i]){
+                System.out.println("what is here - > i " + i  + ", idx " + idx + "," +  candidates[i] + "," + candidates[i - 1]);
+                continue;
+            }
             combination.add(candidates[i]);
-            backtrack(candidates, remaining - candidates[i], target, res, i+1, combination);
-            combination.remove(combination.size()-1);
+            backtrack(candidates, remaining - candidates[i], res, i + 1, combination);
+            combination.remove(combination.size() - 1);
         }
 
     }
---
+++ src/test/java/medium/backtracking/CombinationSumIITest.java
@@ -0,0 +1,17 @@
+package medium.backtracking;
+
+import org.junit.Test;
+import utils.GeneralUtils;
+
+import static org.junit.Assert.assertEquals;
+
+public class CombinationSumIITest {
+    CombinationSumII cs = new CombinationSumII();
+
+    @Test
+    public void test() {
+        assertEquals(GeneralUtils.convertToArrowSeperatedString(
+                cs.combinationSum2(new int[]{10,1,2,7,6,1,5}, 8).stream()
+        ),"[1, 1, 6]->[1, 2, 5]->[1, 7]->[2, 6]");
+    }
+}
