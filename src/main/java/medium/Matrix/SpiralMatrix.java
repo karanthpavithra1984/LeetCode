6082e93c47760807bd18842937d6987e81ee7369
pavithra.holla <pavithra.holla@niceincontact.com>
2025-02-23 23:23:58 -0700

    More Meta leetcode

--- src/main/java/medium/Matrix/SpiralMatrix.java
+++ src/main/java/medium/Matrix/SpiralMatrix.java
@@ -1,4 +1,31 @@
 package medium.Matrix;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+
+        dfs(matrix, result, 0,0);
+        return result;
+    }
+
+    private void dfs(int[][] matrix, List<Integer> result, int row, int col) {
+        if(row < 0 || row >= matrix.length || col < 0
+                || col >= matrix[0].length || matrix[row][col] == 101){
+            return;
+        }
+
+        result.add(matrix[row][col]);
+        matrix[row][col] = 101;
+
+        if(row == 0 || matrix[row-1][col] == 101){
+            dfs(matrix, result, row, col+1);
+        }
+
+        dfs(matrix, result, row+1, col);
+        dfs(matrix, result, row, col-1);
+        dfs(matrix, result, row-1, col);
+    }
 }
---
+++ src/test/java/medium/String/MultiplyStringTest.java
@@ -0,0 +1,15 @@
+package medium.String;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MultiplyStringTest {
+    MultiplyString m = new MultiplyString();
+
+    @Test
+    public void multiply() {
+        assertEquals(m.multiply("123","456"),"56088");
+       assertEquals(m.multiply("3" , "2"),"6");
+    }
+}
