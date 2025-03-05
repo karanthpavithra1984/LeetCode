4938e2ae2256575224b4204a1556d2309e4f1c84
pavithra.holla <pavithra.holla@niceincontact.com>
2025-03-05 08:14:22 -0700

    More meta leet code

---
+++ src/main/java/medium/Trie/WordBreak.java
@@ -0,0 +1,69 @@
+package medium.Trie;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WordBreak {
+
+    /**
+     * Time Complexity o(n2 + m*k)
+     * Space o(n + m*k)
+     * n is the length of the string
+     * m is the number of words
+     * k is the number of characters in the word
+     * @param s
+     * @param wordDict
+     * @return
+     */
+    public boolean wordBreak(String s, List<String> wordDict) {
+        TrieNode head = new TrieNode();
+        //Load Trie
+        for (String word : wordDict) {
+            TrieNode root = head;
+            for (Character c : word.toCharArray()) {
+                if (!root.children.containsKey(c)) {
+                    root.children.put(c, new TrieNode());
+                }
+                root = root.children.get(c);
+            }
+            root.isWord = true;
+        }
+
+        boolean[] dp = new boolean[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            //Do the loop for i == 0
+            //Start looking for next word only if the previous one was a word.
+            if (i != 0 && !dp[i - 1]) {
+                continue;
+            }
+
+            TrieNode curr = head;
+            for (int j = i; j < s.length(); j++) {
+                char c = s.charAt(j);
+                if (!curr.children.containsKey(c)) {
+                    // No words exist
+                    break;
+                }
+
+                curr = curr.children.get(c);
+                if (curr.isWord) {
+                    dp[j] = true;
+                }
+            }
+        }
+
+        return dp[s.length() - 1];
+    }
+}
+
+
+class TrieNode {
+    boolean isWord;
+    Map<Character, TrieNode> children;
+
+    TrieNode() {
+        this.children = new HashMap<>();
+    }
+}
+
---
+++ src/test/java/medium/Trie/WordBreakTest.java
@@ -0,0 +1,29 @@
+package medium.Trie;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class WordBreakTest {
+    WordBreak wordBreak = new WordBreak();
+
+    @Test
+    public void test() {
+        assertTrue(wordBreak.wordBreak("goalspecial",Arrays.asList("go","goal","goals","special")));
+        assertFalse(wordBreak.wordBreak("aaaaaaa", Arrays.asList("aaaa","aa")));
+
+        assertTrue(wordBreak.wordBreak("aaaaaaa",Arrays.asList("aaaa","aaa")));
+
+        assertFalse(wordBreak.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",
+                Arrays.asList("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa")));
+
+
+        assertFalse(wordBreak.wordBreak("catsandog", Arrays.asList("cat","cats",
+                "and","sand","dog")));
+        assertTrue(wordBreak.wordBreak("catsanddog", Arrays.asList("cat","cats",
+                "and","sand","dog")));
+    }
+}
