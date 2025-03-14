fa7485eb62b35395683744c6d92f4696e27651ba
pavithra.holla <pavithra.holla@niceincontact.com>
2025-03-14 00:01:16 -0600

    More binarysearch

---
+++ src/main/java/easy/BinarySearch/TargetIndices.js
@@ -0,0 +1,17 @@
+var targetIndices = function(nums, target) {
+    let countTarget = 0;
+    let lessIndex = 0;
+    for(let num of nums){
+        //Number of Indexes which are lower than target
+        if(num < target) lessIndex++
+        if(num == target) countTarget++;
+    }
+
+    let result = new Array();
+    while(countTarget > 0){
+        result.push(lessIndex++);
+        countTarget--;
+    }
+
+    return result;
+};
