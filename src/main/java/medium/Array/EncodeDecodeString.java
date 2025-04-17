2e38285653ff8f0a6928aad264d2d017db6cc147
pavithra.holla <pavithra.holla@niceincontact.com>
2025-04-17 11:39:26 -0600

    More leetcode

---
+++ src/main/java/medium/Array/EncodeDecodeString.java
@@ -0,0 +1,34 @@
+package medium.Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EncodeDecodeString {
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder res = new StringBuilder();
+        for(String str: strs){
+            res.append(str.length() + "#" + str);
+        }
+
+        return res.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        int start = 0;
+        List<String> values = new ArrayList<>();
+        while(start < s.length()){
+            int temp = start;
+            int number = 0;
+            while(s.charAt(temp) != '#'){
+                number = number *10 +  (s.charAt(temp) - '0');
+                temp++;
+            }
+            values.add(s.substring(temp+1, temp+number+1));
+            start = temp+ number+1;
+        }
+
+        return values;
+    }
+}
---
+++ src/main/java/medium/Array/LongestConsecutiveSequence.java
@@ -0,0 +1,28 @@
+package medium.Array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutiveSequence {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> values = new HashSet<>();
+
+        for(int num: nums){
+            values.add(num);
+        }
+
+        int maxLength = 0;
+        for(int num: nums){
+            if(!values.contains(num-1)){
+                int length = 1;
+                while (values.contains(num + length)) {
+                    length++;
+                }
+
+                maxLength = Math.max(length, maxLength);
+            }
+        }
+
+        return maxLength;
+    }
+}
