0e89df20a66e5790f34c66877116b02edbf5f3f1
pavithra.holla <pavithra.holla@niceincontact.com>
2025-04-26 22:57:49 -0600

    More leetcode

--- src/main/java/hard/Heap/Twitter.java
+++ src/main/java/hard/Heap/Twitter.java
@@ -18,11 +18,16 @@ public class Twitter {
             tweetMap.put(userId, new ArrayList<>());
         }
 
-        tweetMap.get(userId).add(new int[]{count++, tweetId});
+        tweetMap.get(userId).add(new int[] { count++, tweetId });
     }
 
     public List<Integer> getNewsFeed(int userId) {
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> -a[0]));
+
+        if(followerMap.isEmpty()){
+            followerMap.put(userId, new HashSet<>());
+        }
+        followerMap.get(userId).add(userId);
 
         //Get all the followers of the userId
         for(int followee : followerMap.get(userId)){
@@ -30,7 +35,7 @@ public class Twitter {
                 int index = tweetMap.get(followee).size()-1;
                 //Last index
                 int[] tweetInfo = tweetMap.get(followee).get(index);
-                minHeap.offer(new int[]{tweetInfo[0], tweetInfo[1], followee, index-1});
+                minHeap.offer(new int[]{tweetInfo[0], tweetInfo[1], followee, index});
             }
         }
 
@@ -39,21 +44,18 @@ public class Twitter {
             int[] latest = minHeap.poll();
             result.add(latest[1]);
             //Get the next i.e previous to the last of the folowwe and add to the heap
-            int index = latest[3]-1;
+            int index = latest[3];
             if(index > 0) {
-                int[] tweetInfo = tweetMap.get(latest[2]).get(index);
-                minHeap.offer(new int[]{tweetInfo[0], tweetInfo[1]});
+                int[] tweetInfo = tweetMap.get(latest[2]).get(index-1);
+                minHeap.offer(new int[]{tweetInfo[0], tweetInfo[1],latest[2], index-1});
             }
         }
-
-
+        return result;
     }
 
     public void follow(int followerId, int followeeId) {
         if (!followerMap.containsKey(followerId)) {
             followerMap.put(followerId, new HashSet<>());
-            //same followerId to the set
-            followerMap.get(followerId).add(followerId);
         }
 
         followerMap.get(followerId).add(followeeId);
--- src/main/java/medium/backtracking/SubSets.java
+++ src/main/java/medium/backtracking/SubSets.java
@@ -14,21 +14,25 @@ import java.util.List;
 public class SubSets {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
-
         List<Integer> subset = new ArrayList<>();
-        dfs(nums, 0, res, subset);
+
+        dfs(nums, subset, 0, res);
         return res;
     }
 
-    private void dfs(int[] nums, int i, List<List<Integer>> res, List<Integer> subset){
-        if(i >= nums.length) {
-            res.add(new ArrayList<>(subset));
+
+    private void dfs(int[] nums, List<Integer> subset, int index, List<List<Integer>> result){
+        if(index >= nums.length){
+            //Make sure u clone
+            result.add(new ArrayList<>(subset));
             return;
         }
-        subset.add(nums[i]);
-        dfs(nums, i + 1, res, subset);
-        subset.remove(subset.size() - 1);
-        dfs(nums, i + 1, res, subset);
+
+        subset.add(nums[index]);
+        dfs(nums, subset, index+1, result);
+        subset.remove(subset.size()-1);
+        dfs(nums, subset, index+1, result);
     }
+
 
 }
